bf3942e4ee11a2b78775a346e3fc129fa96fa29e
zhaoyunxing <2385585770@qq.com>
2018-05-21 23:56:50 +0800

    修改：md5工具类

--- src/main/java/com/sunny/mongodb/file/util/MD5Util.java
+++ src/main/java/com/sunny/mongodb/file/util/MD5Util.java
@@ -6,37 +6,54 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * MD5 工具类.
- * 
- * @since 1.0.0 2017年3月28日
- * @author <a href="https://waylau.com">Way Lau</a> 
+ * @author sunny
+ * @class: com.sunny.mongodb.file.controller.FileController
+ * @date: 2018-05-18 17:53
+ * @des: md5工具类
  */
 public class MD5Util {
+  private MD5Util() {
+  }
 
-	/** 
-     * 获取该输入流的MD5值 
-     *  
-     * @param is 
-     * @return 
-     * @throws NoSuchAlgorithmException 
-     * @throws IOException 
-     */  
-    public static String getMD5(InputStream is) throws NoSuchAlgorithmException, IOException {  
-        StringBuilder md5 = new StringBuilder();
-        MessageDigest md = MessageDigest.getInstance("MD5");  
-        byte[] dataBytes = new byte[1024];  
-          
-        int nread = 0;   
-        while ((nread = is.read(dataBytes)) != -1) {  
-            md.update(dataBytes, 0, nread);  
-        };  
-        byte[] mdbytes = md.digest();  
-          
-        // convert the byte to hex format  
-        for (byte mdbyte : mdbytes) {
-            md5.append(Integer.toString((mdbyte & 0xff) + 0x100, 16).substring(1));
-        }  
-        return md5.toString();  
-    }  
+  /**
+   * 获取该输入流的MD5值
+   *
+   * @param is
+   * @return
+   * @throws NoSuchAlgorithmException
+   * @throws IOException
+   */
+  public static String getMD5(InputStream is) throws NoSuchAlgorithmException, IOException {
+    StringBuilder md5 = new StringBuilder();
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    byte[] dataBytes = new byte[1024];
 
+    int nread = 0;
+    while ((nread = is.read(dataBytes)) != -1) {
+      md.update(dataBytes, 0, nread);
+    }
+    byte[] mdbytes = md.digest();
+    for (byte mdbyte : mdbytes) {
+      md5.append(Integer.toString((mdbyte & 0xff) + 0x100, 16).substring(1));
+    }
+    return md5.toString();
+  }
+
+  /**
+   * 二进制流加密
+   *
+   * @param bytes
+   * @return
+   * @throws NoSuchAlgorithmException
+   */
+  public static String getMD5(byte[] bytes) throws NoSuchAlgorithmException {
+    StringBuilder md5 = new StringBuilder();
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    md.digest(bytes);
+    byte[] mdbytes = md.digest();
+    for (byte mdbyte : mdbytes) {
+      md5.append(Integer.toString((mdbyte & 0xff) + 0x100, 16).substring(1));
+    }
+    return md5.toString();
+  }
 }
