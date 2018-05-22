ffb70e9865dcef8c44a071cc9749f5eae930f697
zhaoyunxing <2385585770@qq.com>
2018-05-22 15:43:39 +0800

    添加：跨域支持

--- src/main/java/com/sunny/mongodb/file/FileServerMongodbApplication.java
+++ src/main/java/com/sunny/mongodb/file/FileServerMongodbApplication.java
@@ -1,9 +1,12 @@
 package com.sunny.mongodb.file;
 
+import com.sunny.mongodb.file.config.FileWebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({FileWebMvcConfigurer.class})
 public class FileServerMongodbApplication {
 
   public static void main(String[] args) {
