5a68a25ed204239fffbd416f32d7b18817dfe185
zhaoyunxing <2385585770@qq.com>
2018-05-22 18:36:38 +0800

    修改：file类

--- src/main/java/com/sunny/mongodb/file/model/File.java
+++ src/main/java/com/sunny/mongodb/file/model/File.java
@@ -1,6 +1,9 @@
 package com.sunny.mongodb.file.model;
 
+import org.bson.types.Binary;
+
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author sunny
@@ -9,23 +12,45 @@ import java.io.Serializable;
  * @des: 文件
  */
 
-public class File extends Document implements Serializable {
+public class File implements Serializable {
   private static final long serialVersionUID = 3718103350794331126L;
   /**
-   * 文档id
+   * 主键id
    */
-  private String docId;
+  private String id;
   /**
    * 文件名称
    */
   private String name;
 
-  public String getDocId() {
-    return docId;
+  /**
+   * 文件md5
+   */
+  private String md5;
+  /**
+   * 文件大小
+   */
+  private Integer size;
+  /**
+   * 文件内容
+   */
+  private Binary content;
+
+  /**
+   * 文件类型
+   */
+  private String contentType;
+  /**
+   * 上传时间
+   */
+  private Date uploadDate;
+
+  public String getId() {
+    return id;
   }
 
-  public void setDocId(String docId) {
-    this.docId = docId;
+  public void setId(String id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -36,17 +61,56 @@ public class File extends Document implements Serializable {
     this.name = name;
   }
 
+  public String getMd5() {
+    return md5;
+  }
+
+  public void setMd5(String md5) {
+    this.md5 = md5;
+  }
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+
+  public Binary getContent() {
+    return content;
+  }
+
+  public void setContent(Binary content) {
+    this.content = content;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
+
+  public Date getUploadDate() {
+    return uploadDate;
+  }
+
+  public void setUploadDate(Date uploadDate) {
+    this.uploadDate = uploadDate;
+  }
+
   @Override
   public String toString() {
     return "File{" +
-        "docId='" + docId + '\'' +
+        "id='" + id + '\'' +
         ", name='" + name + '\'' +
-        ", id='" + getId() + '\'' +
-        ", md5='" + getMd5() + '\'' +
-        ", size=" + getSize() +
-        ", content=" + getContent() +
-        ", contentType='" + getContentType() + '\'' +
-        ", uploadDate=" + getUploadDate() +
-        "} ";
+        ", md5='" + md5 + '\'' +
+        ", size=" + size +
+        ", content=" + content +
+        ", contentType='" + contentType + '\'' +
+        ", uploadDate=" + uploadDate +
+        '}';
   }
 }
